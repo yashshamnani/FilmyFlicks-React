a7b32a531eb26a830870eca1e19021226ac62de1
Yash Shamnani <shyamnaniyash@gmail.com>
2023-05-07 12:27:13 +0530

    header

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,21 @@
-//  import React from 'react'
-import HeroBanner from "./heroBanner/HeroBanner";
+import React from "react";
+
 import "./style.scss";
- 
- const Home = () => {
-   return (
-     <div>
-        <HeroBanner />
-     </div>
-   )
- }
- 
- export default Home
- 
+
+import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
+import Popular from "./popular/Popular";
+import TopRated from "./topRated/TopRated";
+
+const Home = () => {
+    return (
+        <div className="homePage">
+            <HeroBanner />
+            <Trending />
+            <Popular />
+            <TopRated />
+        </div>
+    );
+};
+
+export default Home;
--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -9,34 +9,34 @@ import Img from "../../../components/lazyLoadImage/Img";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 
 const HeroBanner = () => {
-  const [background, setBackground] = useState("");
-  const [query, setQuery] = useState("");
-  const navigate = useNavigate();
-  const {url} = useSelector((state)=> state.home)
-  const {data,loading} = useFetch("/movie/upcoming")
+    const [background, setBackground] = useState("");
+    const [query, setQuery] = useState("");
+    const navigate = useNavigate();
+    const { url } = useSelector((state) => state.home);
+    const { data, loading } = useFetch("/movie/upcoming");
 
-  useEffect(()=>{
-       const bg = url.backdrop + data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
-       setBackground(bg)
-  },[data])
+    useEffect(() => {
+        const bg =
+            url.backdrop +
+            data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+        setBackground(bg);
+    }, [data]);
 
+    const searchQueryHandler = (event) => {
+        if (event.key === "Enter" && query.length > 0) {
+            navigate(`/search/${query}`);
+        }
+    };
 
-  const searchQueryHandler = (event) => {
-    if (event.key === "Enter" && query.length > 0) {
-      navigate(`/search/${query}`);
-    }
-  };
-
-  return (
-    <div className="heroBanner">
-         {!loading && (
+    return (
+        <div className="heroBanner">
+            {!loading && (
                 <div className="backdrop-img">
                     <Img src={background} />
                 </div>
             )}
 
-
-<div className="opacity-layer"></div>
+            <div className="opacity-layer"></div>
             <ContentWrapper>
                 <div className="heroBannerContent">
                     <span className="title">Welcome.</span>
@@ -56,7 +56,7 @@ const HeroBanner = () => {
                 </div>
             </ContentWrapper>
         </div>
-  );
+    );
 };
 
 export default HeroBanner;
